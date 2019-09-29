4051bba81409fe0629722ba4c180f168533964a3
Christian Benjaminsson <christiansdator@Christians-MacBook-Pro.local>
2019-09-29 11:26:33 +0200

    Push project file

---
+++ JavaArKul.java
@@ -0,0 +1,9 @@
+package out.example.javaarkul.;
+
+public class JavaArKul {
+    public static void main(String[] args){
+        for(int i=0; i<=3; i++){
+            System.out.println("Java is fun");
+        }
+    }
+}
